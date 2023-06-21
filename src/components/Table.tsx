75b66ffe87e7037c0393462e332cef289c21db8e
guillaume <guillaume.richer@metav.rs>
2023-06-21 17:46:46 +0200

    fix bug on table hydratation

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,4 @@
-import { React } from "react";
+import React from "react";
 
 const Table = () => {
   const data = [
@@ -7,7 +7,7 @@ const Table = () => {
       name: "Bitcoin",
       price: 19,
       hourly: "",
-      dayly: "0.7%",
+      daily: "0.7%",
       weekly: "1.13%",
       marketCap: "40000",
       volume: "4000",
@@ -18,7 +18,7 @@ const Table = () => {
       name: "Bitcoin",
       price: 19,
       hourly: "",
-      dayly: "0.7%",
+      daily: "0.7%",
       weekly: "1.13%",
       marketCap: "40000",
       volume: "4000",
@@ -29,7 +29,7 @@ const Table = () => {
       name: "Bitcoin",
       price: 19,
       hourly: "",
-      dayly: "0.7%",
+      daily: "0.7%",
       weekly: "1.13%",
       marketCap: "40000",
       volume: "4000",
@@ -40,7 +40,7 @@ const Table = () => {
       name: "Bitcoin",
       price: 19,
       hourly: "",
-      dayly: "0.7%",
+      daily: "0.7%",
       weekly: "1.13%",
       marketCap: "40000",
       volume: "4000",
@@ -51,42 +51,43 @@ const Table = () => {
       name: "Bitcoin",
       price: 19,
       hourly: "",
-      dayly: "0.7%",
+      daily: "0.7%",
       weekly: "1.13%",
       marketCap: "40000",
       volume: "4000",
       supply: "10 BTC",
     },
   ];
+
   return (
     <div>
       <table>
-        <tr>
-          <th>#</th>
-          <th>Nom</th>
-          <th>Prix</th>
-          <th>1h %</th>
-          <th>% 24h</th>
-          <th>7d %</th>
-          <th>Cap. Marché</th>
-          <th>Volume (24 h)</th>
-          <th>Offre en Circulation</th>
-        </tr>
-        {data.map((val, key) => {
-          return (
+        <tbody>
+          <tr>
+            <th>#</th>
+            <th>Nom</th>
+            <th>Prix</th>
+            <th>1h %</th>
+            <th>% 24h</th>
+            <th>7d %</th>
+            <th>Cap. Marché</th>
+            <th>Volume (24 h)</th>
+            <th>Offre en Circulation</th>
+          </tr>
+          {data.map((val, key) => (
             <tr key={key}>
               <td>{val.rank}</td>
               <td>{val.name}</td>
               <td>{val.price}</td>
               <td>{val.hourly}</td>
-              <td>{val.dayly}</td>
+              <td>{val.daily}</td>
               <td>{val.weekly}</td>
               <td>{val.marketCap}</td>
               <td>{val.volume}</td>
               <td>{val.supply}</td>
             </tr>
-          );
-        })}
+          ))}
+        </tbody>
       </table>
     </div>
   );
